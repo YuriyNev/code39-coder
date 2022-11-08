75236b58574e696c69b6249877afa7ad63d89f69
Юрий <yu.nev@ya.ru>
2022-11-08 22:45:39 +0500

    Simple unit test: * factory using

---
+++ Coder39/Types/Strip.cs
@@ -0,0 +1,18 @@
+﻿namespace Aspose.Coder39.Types;
+
+public class Strip
+{
+    public byte Size { get; }
+    
+    public Color? Color { get; private set; }
+
+    public Strip(byte size)
+    {
+        if (size <= 0) throw new ArgumentOutOfRangeException(nameof(size));
+        if (size > 3) throw new ArgumentOutOfRangeException(nameof(size));
+        
+        Size = size;
+    }
+
+    public void Fill(Color color) => Color = color;
+}
