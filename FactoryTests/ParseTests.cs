99d05e7f54bc04e9092fdc5e25a11dec39d7a63b
Юрий <yu.nev@ya.ru>
2022-11-09 22:15:31 +0500

    New parse tests

--- FactoryTests/ParseTests.cs
+++ FactoryTests/ParseTests.cs
@@ -1,6 +1,20 @@
-﻿namespace TestProject1;
+﻿using Aspose.Coder39.Services;
+
+namespace TestProject1;
 
 public class ParseTests
 {
+    [Fact]
+    public void BarParseFailedTooWideSpaceTest()
+    {
+        Assert.Throws<InvalidTextBarException>(() => "111010100000000010111".Parse());
+    }
     
+    [Fact]
+    public void BarParse1Test()
+    {
+        var bar = "111010100010111".Parse();
+        
+        Assert.NotNull(bar);
+    }
 }
